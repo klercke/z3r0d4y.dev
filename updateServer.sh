0d8668e31becb030e8ee9cec49977fea1687b822
klercke <klercke@protonmail.com>
2020-03-05 16:43:44 -0500

    add updateServer.sh

---
+++ updateServer.sh
@@ -0,0 +1,3 @@
+sudo cp index.html /srv/http/prototypexenon.com/public_html/index.html
+sudo cp about.html /srv/http/prototypexenon.com/public_html/about.html
+sudo cp styles.css /srv/http/prototypexenon.com/public_html/styles.css
