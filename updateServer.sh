b012705022314cf25462c9d46e9f12b78b992238
klercke <klercke@protonmail.com>
2020-03-06 13:49:04 -0500

    update updater script

--- updateServer.sh
+++ updateServer.sh
@@ -1,3 +1,7 @@
+sudo rm /srv/http/prototypexenon.com/public_html/index.html
+sudo rm /srv/http/prototypexenon.com/public_html/styles.css
+sudo rm /srv/http/prototypexenon.com/public_html/scripts.js
+
 sudo cp index.html /srv/http/prototypexenon.com/public_html/index.html
-sudo cp about.html /srv/http/prototypexenon.com/public_html/about.html
+sudo cp scripts.js /srv/http/prototypexenon.com/public_html/scripts.js
 sudo cp styles.css /srv/http/prototypexenon.com/public_html/styles.css
