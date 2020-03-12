418fbb8c9d5b0d47c4bd965622f5101dbc74edbd
Konnor Klercke <klercke@protonmail.com>
2020-03-12 19:40:06 -0400

    add command-line functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
-function addAboutContent() {
-    var blink = document.getElementsByClassName('blink');
-    for (var i = 0; i < blink.length; i++) {
-        blink[i].remove();
-    }
-
-
+function printAbout() {
+    removePrompt();
 
     document.body.innerHTML = document.body.innerHTML + `
         <br> [konnor.klercke@prototypexenon.com ~]# cat about.txt 
@@ -14,5 +9,71 @@ function addAboutContent() {
         <br> About: {
         <br> &emsp;&emsp;&emsp;&emsp; My name is Konnor and I like placeholder text. I'll fill this in later.
         <br> &emsp;&emsp;&emsp;&emsp; }
-        <div class="blink">[konnor.klercke@prototypexenon.com ~]# </div>`;
+    `;
+
+    addPrompt();
+}
+
+function ls() {
+    removePrompt();
+
+    document.body.innerHTML = document.body.innerHTML + `
+        <br> [konnor.klercke@prototypexenon.com ~]# ls
+        <br> total 1
+        <br> -rw-r--r-- 1 konnor.klercke konnor.klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.txt" onclick="printAbout();">about.txt</button>
+    `;
+
+    addPrompt();
+}
+
+
+function unknownCommand(command) {
+    removePrompt();
+
+    document.body.innerHTML = document.body.innerHTML + `
+    <br> [konnor.klercke@prototypexenon.com ~]# ` + command + `
+    <br> bash: ` + command + `: command not found
+    `;
+    addPrompt();
+}
+
+
+window.onload = function init() {
+    addPrompt();
+    ls();
+}
+
+function removePrompt() {
+    var blink = document.getElementsByClassName('blink');
+    for (var i = 0; i < blink.length; i++) {
+        blink[i].remove();
+    }
+}
+
+function addPrompt() {
+    document.body.innerHTML = document.body.innerHTML + `
+        <div class="blink">[konnor.klercke@prototypexenon.com ~]# <form> <input autocomplete="off" size="1" type="text" id="command"/> <input type="submit" style="display: none;" onclick="interpretCommand()" /> </form> </div>
+    `;
+
+    var input = document.getElementsByTagName('input')[0];
+
+    input.addEventListener("keydown", function(){
+        this.size = this.value.length + 1;
+    });
+
+    input.select();
+}
+
+function interpretCommand() {
+    var command = document.getElementById('command').value;
+    console.log(command);
+
+    switch (command){
+        case 'ls':
+            ls();
+            break;
+        default:
+            unknownCommand(command);
+            break;
+    }
 }
