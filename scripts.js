197e052fa7be29a1854a7137956fd19fb2a4a6c4
Konnor Klercke <klercke@protonmail.com>
2020-03-12 22:14:32 -0400

    add help command

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 function printAbout() {
-    removePrompt();
-
     document.body.innerHTML = document.body.innerHTML + `
         <br> [konnor.klercke@prototypexenon.com ~]# cat about.txt 
         <br>
@@ -10,37 +8,50 @@ function printAbout() {
         <br> &emsp;&emsp;&emsp;&emsp; My name is Konnor and I like placeholder text. I'll fill this in later.
         <br> &emsp;&emsp;&emsp;&emsp; }
     `;
-
-    addPrompt();
 }
 
 function ls() {
-    removePrompt();
-
     document.body.innerHTML = document.body.innerHTML + `
         <br> [konnor.klercke@prototypexenon.com ~]# ls
         <br> total 1
-        <br> -rw-r--r-- 1 konnor.klercke konnor.klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.txt" onclick="printAbout();">about.txt</button>
+        <br> -rw-r--r-- 1 konnor.klercke konnor.klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.txt" onclick="interpretCommand('cat about.txt');">about.txt</button>
     `;
-
-    addPrompt();
 }
 
+function cat(file) {
+    switch (file) {
+        case "about.txt":
+            printAbout();
+            break;
+        default:
+            document.body.innerHTML = document.body.innerHTML + `
+            <br> [konnor.klercke@prototypexenon.com ~]# cat ` + file + `
+            <br> cat: ` + file + `: No such file or directory
+            `;
+            break;
+    }    
+}
 
-function unknownCommand(command) {
-    removePrompt();
+function clear() {
+    document.body.innerHTML = "";
+}
 
+function help() {
     document.body.innerHTML = document.body.innerHTML + `
-    <br> [konnor.klercke@prototypexenon.com ~]# ` + command + `
-    <br> bash: ` + command + `: command not found
+        <br> [konnor.klercke@prototypexenon.com ~]# help
+        <br> ProtOS smash v1.1.1-release (silly fake website terminal)
+        <br> These shell commands are defined internally. Type 'help' to see this list.
+        <br>
+        <br> ls - lists all files and directories in the current working directory.
+        <br> help - prints this list.
+        <br> clear - clears the screen.
+        <br> cat [file] - prints the contents of 'file'.
     `;
-    addPrompt();
 }
-
 
 window.onload = function init() {
-    addPrompt();
     ls();
+    addPrompt();
 }
 
 function removePrompt() {
@@ -64,16 +75,39 @@ function addPrompt() {
     input.select();
 }
 
-function interpretCommand() {
-    var command = document.getElementById('command').value;
-    console.log(command);
+function interpretCommand(input) {
+    if (!input) {
+        var command = document.getElementById('command').value;
+        var rawInput = command;
+        command = command.split(" ");
+    }
+    else {
+        console.log(input);
+        var command = input.split(" ");
+    }
+    
 
-    switch (command){
+    removePrompt();
+    switch (command[0]){
         case 'ls':
             ls();
             break;
+        case 'cat':
+            cat(command[1]);
+            break;
+        case 'clear':
+            clear();
+            break;
+        case 'help':
+            help();
+            break;
         default:
-            unknownCommand(command);
+            document.body.innerHTML = document.body.innerHTML + `
+            <br> [konnor.klercke@prototypexenon.com ~]# ` + rawInput + `
+            <br> smash: ` + command[0] + `: command not found
+            `;
             break;
     }
+
+    addPrompt();
 }
