c4634e5333b335232dabc36dbaf58c266a801517
Konnor Klercke <klercke@protonmail.com>
2020-11-27 20:45:04 -0500

    add wishlist

--- updateServer.sh
+++ updateServer.sh
@@ -1,7 +1,9 @@
 sudo rm /srv/http/prototypexenon.com/public_html/index.html
 sudo rm /srv/http/prototypexenon.com/public_html/styles.css
 sudo rm /srv/http/prototypexenon.com/public_html/scripts.js
+sudo rm /srv/http/prototypexenon.com/public_html/wishlist.html
 
 sudo cp index.html /srv/http/prototypexenon.com/public_html/index.html
 sudo cp scripts.js /srv/http/prototypexenon.com/public_html/scripts.js
 sudo cp styles.css /srv/http/prototypexenon.com/public_html/styles.css
+sudo cp wishlist.html /srv/http/prototypexenon.com/public_html/wishlist.html
