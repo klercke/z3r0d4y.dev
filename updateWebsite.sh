41187033e543c24178157c5d1dd7fd42a1e0a009
Konnor Klercke <klercke@prototypexenon.com>
2021-04-19 20:44:00 -0400

    update updateWebsite.sh

--- updateWebsite.sh
+++ updateWebsite.sh
@@ -1,9 +1,8 @@
 sudo rm /srv/http/prototypexenon.com/public_html/index.html
 sudo rm /srv/http/prototypexenon.com/public_html/styles.css
 sudo rm /srv/http/prototypexenon.com/public_html/scripts.js
-sudo rm /srv/http/prototypexenon.com/public_html/wishlist.html
 
 sudo cp index.html /srv/http/prototypexenon.com/public_html/index.html
 sudo cp scripts.js /srv/http/prototypexenon.com/public_html/scripts.js
 sudo cp styles.css /srv/http/prototypexenon.com/public_html/styles.css
-sudo cp wishlist.html /srv/http/prototypexenon.com/public_html/wishlist.html
+sudo cp crypto/* /srv/http/prototypexenon.com/public_html/
