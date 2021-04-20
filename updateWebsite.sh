a83af8a2be78131df93df7498f78bc36d57ed3cc
Konnor Klercke <klercke@prototypexenon.com>
2021-04-19 20:44:49 -0400

    update updateWebsite.sh

--- updateWebsite.sh
+++ updateWebsite.sh
@@ -5,4 +5,4 @@ sudo rm /srv/http/prototypexenon.com/public_html/scripts.js
 sudo cp index.html /srv/http/prototypexenon.com/public_html/index.html
 sudo cp scripts.js /srv/http/prototypexenon.com/public_html/scripts.js
 sudo cp styles.css /srv/http/prototypexenon.com/public_html/styles.css
-sudo cp crypto/* /srv/http/prototypexenon.com/public_html/
+sudo cp crypto/* /srv/http/prototypexenon.com/public_html/crypto
