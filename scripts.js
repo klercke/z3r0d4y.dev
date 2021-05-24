12ac7af945cb6196504730d45218c64109946e08
Konnor Klercke <klercke@prototypexenon.com>
2021-05-24 16:10:35 -0400

    Update about section

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
 function printAbout() {
     document.body.innerHTML = document.body.innerHTML + `
-        <br> [konnor.klercke@prototypexenon.com ~]# cat about.txt 
+        <br> [klercke@prototypexenon.com ~]# cat about.txt 
         <br>
         <br> Name: &emsp;&nbsp; "Konnor Otto Klercke"
         <br> Email: &emsp; "klercke@prototypexenon.com"
         <br> About: {
-        <br> 	My name is Konnor and I like placeholder text. I'll fill this in later.
+        <br> 	My name is Konnor Klercke and this is my website.
+        <br>    I'm a FOSS (free and open source software) enthusiast, and an infosec / privacy hobbyist.
+        <br>    I spend a lot of my free time developing small scripts or programs for personal use, researching security concepts, and developing my personal WIP Linux distro, ProtOS.
+        <br>    I use Arch Linux full time on most of my machines, with the exception of my desktop, which dual boots Arch Linux and Windows 10 for gaming.
+        <br>    I love learning about retro tech as well as talking to people with unique experiences, so feel free to shoot me an email if you have some interesting tech stories to share.
+        <br>    Outside of the tech world, I love to go camping and hiking, a passion which I got through my experience earning my Eagle Scout award.
+        <br>    Cooking is another hobby of mine, especially customizing recipes to change things up.
+        <br>    I've also lived in quite a few places in my relatively short time on this Earth, including Baltimore, Rochester, Stockholm, Cincinnati, Kentucky, Louisville, and Nashville.
+        <br>    One of my goals in life is to visit every National Park in the United States.
         <br> }
     `;
 }
 
 function ls() {
-    document.body.innerHTML = document.body.innerHTML + `[konnor.klercke@prototypexenon.com ~]#  ls
+    document.body.innerHTML = document.body.innerHTML + `[klercke@prototypexenon.com ~]#  ls
 total 1
--rw-r--r-- 1 konnor.klercke konnor.klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.txt" onclick="interpretCommand('cat about.txt');">about.txt</button>
+-rw-r--r-- 1 klercke klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.txt" onclick="interpretCommand('cat about.txt');">about.txt</button>
 `;
 }
 
@@ -24,7 +32,7 @@ function cat(file) {
             break;
         default:
             document.body.innerHTML = document.body.innerHTML + `
-            <br> [konnor.klercke@prototypexenon.com ~]#  cat ` + file + `
+            <br> [klercke@prototypexenon.com ~]#  cat ` + file + `
             <br> cat: ` + file + `: No such file or directory
             `;
             break;
@@ -37,7 +45,7 @@ function clear() {
 
 function help() {
     document.body.innerHTML = document.body.innerHTML + `
-        [konnor.klercke@prototypexenon.com ~]#  help
+        [klercke@prototypexenon.com ~]#  help
         ProtOS smash v2.1.0-release (silly fake website terminal)
         These shell commands are defined internally. Type 'help' to see this list.
         
@@ -120,7 +128,7 @@ function interpretCommand(input) {
             break;
         default:
             document.body.innerHTML = document.body.innerHTML + `
-            <br> [konnor.klercke@prototypexenon.com ~]# ` + rawInput + `
+            <br> [klercke@prototypexenon.com ~]# ` + rawInput + `
             <br> smash: ` + command[0] + `: command not found
             `;
             break;
