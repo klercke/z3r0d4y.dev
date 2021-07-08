8ca48b59e4962d501e51c40730e64259301c8635
Konnor Klercke <klercke@prototypexenon.com>
2021-07-07 23:32:33 -0400

    Improve command output formatting

--- scripts.js
+++ scripts.js
@@ -1,40 +1,38 @@
-function printAbout() {
-    document.body.innerHTML = document.body.innerHTML + `
-        <br> [klercke@prototypexenon.com ~]# cat about.txt 
-        <br>
-        <br> Name: &emsp;&nbsp; "Konnor Otto Klercke"
-        <br> Email: &emsp; "klercke@prototypexenon.com"
-        <br> About: {
-        <br> 	My name is Konnor Klercke and this is my website.
-        <br>    I'm a FOSS (free and open source software) enthusiast, and an infosec / privacy hobbyist.
-        <br>    I spend a lot of my free time developing small scripts or programs for personal use, researching security concepts, and developing my personal WIP Linux distro, ProtOS.
-        <br>    I use Arch Linux full time on most of my machines, with the exception of my desktop, which dual boots Arch Linux and Windows 10 for gaming.
-        <br>    I love learning about retro tech as well as talking to people with unique experiences, so feel free to shoot me an email if you have some interesting tech stories to share.
-        <br>    Outside of the tech world, I love to go camping and hiking, a passion which I got through my experience earning my Eagle Scout award.
-        <br>    Cooking is another hobby of mine, especially customizing recipes to change things up.
-        <br>    I've also lived in quite a few places in my relatively short time on this Earth, including Baltimore, Rochester, Stockholm, Cincinnati, Kentucky, Louisville, and Nashville.
-        <br>    One of my goals in life is to visit every National Park in the United States.
-        <br> }
-    `;
+function catAbout() {
+    document.body.innerHTML = document.body.innerHTML +
+        `[klercke@prototypexenon.com ~]# cat about.json\r\n` +
+        `\r\n` +
+        `Name: &emsp;&nbsp; "Konnor Otto Klercke"\r\n` +
+        `Email: &emsp; "klercke@prototypexenon.com"\r\n` +
+        `About: {\r\n` +
+        `   My name is Konnor Klercke and this is my website.\r\n` +
+        `   I'm a FOSS (free and open source software) enthusiast, and an infosec / privacy hobbyist.\r\n` +
+        `   I spend a lot of my free time developing small scripts or programs for personal use, researching security concepts, and developing my personal WIP Linux distro, ProtOS.\r\n` +
+        `   I use Arch Linux full time on most of my machines, with the exception of my desktop, which dual boots Arch Linux and Windows 10 for gaming.\r\n` +
+        `   I love learning about retro tech as well as talking to people with unique experiences, so feel free to shoot me an email if you have some interesting tech stories to share.\r\n` +
+        `   Outside of the tech world, I love to go camping and hiking, a passion which I got through my experience earning my Eagle Scout award.\r\n` +
+        `   Cooking is another hobby of mine, especially customizing recipes to change things up.\r\n` +
+        `   I've also lived in quite a few places in my relatively short time on this Earth, including Baltimore, Rochester, Stockholm, Cincinnati, Kentucky, Louisville, and Nashville.\r\n` +
+        `   One of my goals in life is to visit every National Park in the United States. ` +
+        `}\r\n`;
+
 }
 
 function ls() {
-    document.body.innerHTML = document.body.innerHTML + `[klercke@prototypexenon.com ~]#  ls
-total 1
--rw-r--r-- 1 klercke klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.txt" onclick="interpretCommand('cat about.txt');">about.txt</button>
-`;
+    document.body.innerHTML += `[klercke@prototypexenon.com ~]# ls\r\n` +
+        `total 1\r\n` + 
+        `-rw-r--r-- 1 klercke klercke &emsp; 4096 Mar 5 14:58 <button type="button" value="about.json" onclick="interpretCommand('cat about.json');">about.json</button>\r\n`;
 }
 
 function cat(file) {
     switch (file) {
-        case "about.txt":
-            printAbout();
+        case "about.json":
+            catAbout();
             break;
         default:
-            document.body.innerHTML = document.body.innerHTML + `
-            <br> [klercke@prototypexenon.com ~]#  cat ` + file + `
-            <br> cat: ` + file + `: No such file or directory
-            `;
+            document.body.innerHTML +=
+            `[klercke@prototypexenon.com ~]#  cat ` + file + `\r\n` +
+            `cat: ` + file + `: No such file or directory\r\n`;
             break;
     }    
 }
@@ -44,16 +42,14 @@ function clear() {
 }
 
 function help() {
-    document.body.innerHTML = document.body.innerHTML + `
-        [klercke@prototypexenon.com ~]#  help
-        ProtOS smash v2.1.0-release (silly fake website terminal)
-        These shell commands are defined internally. Type 'help' to see this list.
-        
-        ls - lists all files and directories in the current working directory.
-        help - prints this list.
-        clear - clears the screen.
-        cat [file] - prints the contents of 'file'.
-    `;
+    document.body.innerHTML += `[klercke@prototypexenon.com ~]# help\r\n` + 
+        `ProtOS smash v2.1.0-release (silly fake website terminal)\r\n` +
+        `These shell commands are defined internally. Type 'help' to see this list.\r\n` +
+        `\r\n`+
+        `ls - lists all files and directories in the current working directory.\r\n` +
+        `help - prints this list.\r\n` +
+        `clear - clears the screen.\r\n` +
+        `cat [file] - prints the contents of 'file'.\r\n`;
 }
 
 window.onload = function init() {
@@ -134,7 +130,6 @@ window.onload = function init() {
             else
                 document.getElementById('cursorCharacter').innerText = "";
 		}
-        console.log(inputBuffer);
 	});
 }
 
@@ -144,17 +139,19 @@ function removePrompt() {
 }
 
 function addPrompt() {
-    document.body.innerHTML = document.body.innerHTML + `<span id='prompt'>[klercke@prototypexenon.com ~]# <span id='userInput'></span><span id='cursorCharacter'></span><span id='userInput2'></span></span>`;
+    document.body.innerHTML += `<span id='prompt'>[klercke@prototypexenon.com ~]# <span id='userInput'></span><span id='cursorCharacter'></span><span id='userInput2'></span></span>`;
 	
-	document.getElementById('userInput').innerHTML = "&nbsp;";
+	document.getElementById('userInput').innerHTML = "";
 }
 
 function interpretCommand(input) {
+    
     if (!input) {
         var command = document.getElementById('command').value;
         var rawInput = command;
         command = command.split(" ");
     }
+    
     else {
     	console.log(input);
         var command = input.split(" ");
@@ -176,10 +173,9 @@ function interpretCommand(input) {
             help();
             break;
         default:
-            document.body.innerHTML = document.body.innerHTML + `
-            <br> [klercke@prototypexenon.com ~]# ` + rawInput + `
-            <br> smash: ` + command[0] + `: command not found
-            `;
+            document.body.innerHTML = document.body.innerHTML + 
+                `[klercke@prototypexenon.com ~]# ` + command + `\r\n` +
+                `smash: ` + command[0] + `: command not found\r\n`;
             break;
     }
 
